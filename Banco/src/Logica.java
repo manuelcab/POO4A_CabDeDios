846e572cbb681314abae83be64b67a0b9ff75c41
manuelcab <manuel110010101011@hotmail.com>
2024-02-21 01:05:26 -0600

    Inicio

---
+++ Banco/src/Logica.java
@@ -0,0 +1,10 @@
+public class Logica {
+
+    public static void main(String[] args) {
+
+        Interfaz pantalla = new Interfaz();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        
+    }
+}
